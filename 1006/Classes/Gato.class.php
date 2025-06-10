5058b59ecd6765e597fd241934f171f2d372c68b
Ana Castro de Araújo <anacabh@gmail.com>
2025-06-10 10:16:10 -0300

    foi memo

---
+++ 1006/Classes/Gato.class.php
@@ -0,0 +1,23 @@
+<?php
+class Gato{
+    public $Raca;
+    public $Cor;
+    public $Idade;
+    public $Peso;
+    public $Porte;
+    public $Nome;
+    public $Sexo;
+
+    public function miar(){
+        return " Miau!";
+}
+    public function dormir(){
+    return "Dormindo";
+}
+    public function comer(){
+        return "Comendo";
+}
+}
+
+
+?>
--- 1006/oop.php
+++ 1006/oop.php
@@ -1,10 +1,12 @@
 <?php
 include_once './Classes/Pessoa.class.php';
-
 $pessoa = New Pessoa();
-
 $pessoa->Nome = 'Ana';
 $pessoa->Idade = 16;
-
 echo $pessoa->apresentar();
+
+include_once './Classes/Gato.class.php';
+$gato = New Gato();
+$gato->Cor = 'Rosa choque';
+echo $gato->miar();
 ?>
